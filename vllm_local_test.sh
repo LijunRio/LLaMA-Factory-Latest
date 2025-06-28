8f742b96aeeb66db21c98abfb98f88b01325d592
LijunRio <rioin1998@163.com>
2025-06-28 11:39:37 +0200

    update

--- vllm_local_test.sh
+++ vllm_local_test.sh
@@ -12,11 +12,9 @@
 python scripts/vllm_infer.py \
     --model_name_or_path /home/june/Code/new_llamafactory/saves/huggingface_origin/InternVL3-8B\
     --dataset coco_test\
-    ---trust-remote-code true \
     --template intern_vl\
     --save_name "evaluate_outputs/zero_shot_intervlm/generated_predictions_llamafactory_vlm.jsonl"\
     --max_new_tokens 1024 \
-     --use_cache true \
     --temperature 0.0
 
 # python scripts/my_vllm_infer.py \
