de417a89d42f403009c8dfcba74fe3212f1f8fdd
LijunRio <rioin1998@163.com>
2025-06-30 20:45:06 +0200

    upeate

---
+++ batch_inference.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+declare -a FILES=("vindr_sft_base")  # 修改为实际的目录名
+EVAL_DATASET="vinder_adkg_test"
+declare -a MODELS=("qwen2_vl-3b")  # 修改为实际的模型名
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+
+for model_name in "${MODELS[@]}"; do
+    # 设置路径
+    CHECKPOINT_DIR="saves/$model_name/${FILES[0]}"
+    OUTPUT_DIR="./evaluate_outputs/train_results/${EVAL_DATASET}__${TIMESTAMP}/${FILES[0]}/$model_name"
+    echo "Checkpoint directory: $CHECKPOINT_DIR"
+    echo "Output directory: $OUTPUT_DIR"
+
+    echo "Processing model: $model_name"
+    mkdir -p "$OUTPUT_DIR"
+
+    # 遍历每个 checkpoint
+    for ckpt in $CHECKPOINT_DIR/checkpoint-*; do
+        if [ -d "$ckpt" ]; then
+            ckpt_name=$(basename "$ckpt")
+            echo "Running inference on checkpoint: $ckpt_name"
+
+            save_path="$OUTPUT_DIR/$ckpt_name"
+            mkdir -p "$save_path"
+
+            # 执行 Python 脚本进行推理 (使用贪婪搜索进行测试)
+            python scripts/vllm_infer.py \
+                --model_name_or_path "$ckpt" \
+                --dataset $EVAL_DATASET \
+                --template qwen2_vl \
+                --save_name "$save_path/generated_predictions.jsonl" \
+                --max_new_tokens 4096 \
+                --temperature 0.0 \
+                --top_p 1.0 \
+                --top_k -1 \
+                --repetition_penalty 1.0 \
+                --seed 42
+        fi
+    done
+done
---
+++ evaluate_outputs/03_new_metric.py
@@ -0,0 +1,18 @@
+"""
+
+我要新写一个代码，用于计算grounding的各种指标
+map30
+map50
+map75 
+map 50 95
+ACC30
+ACC50
+ACC50
+Recall30
+Recall50
+Shape matching
+Detection
+
+
+
+"""
