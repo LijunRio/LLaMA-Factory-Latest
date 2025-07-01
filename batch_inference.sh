9fe65170a86cf06e44304300eb1030290dccf9c3
LijunRio <rioin1998@163.com>
2025-07-01 18:02:11 +0200

    update

--- batch_inference.sh
+++ batch_inference.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+### 一定要使用VLLM_USE_V1=0, 用vllm v0版本https://github.com/vllm-project/vllm/pull/16618
+## --enforce-eager 或设置 VLLM_USE_V1=0 则恢复正常输出。 vLLM v1 中对某些特殊结构（如 GLM4 的 residual 处理、KV-cache 策略、多模态输入）没有正确兼容，导致输出逻辑异常。
 
 declare -a FILES=("vindr_sft_base")  # 修改为实际的目录名 vindr_sft_def,vindr_sft_base
 EVAL_DATASET="vinder_adkg_test" # vinder_adkg_def_test,vinder_adkg_test
@@ -29,7 +31,7 @@ for model_name in "${MODELS[@]}"; do
             mkdir -p "$save_path"
 
             # 执行 Python 脚本进行推理 (使用贪婪搜索进行测试)
-            python scripts/vllm_infer_new.py \
+             env VLLM_USE_V1=0 python scripts/vllm_infer_new.py \
                 --model_name_or_path "$ckpt" \
                 --dataset $EVAL_DATASET \
                 --template qwen2_vl \
@@ -39,7 +41,7 @@ for model_name in "${MODELS[@]}"; do
                 --top_p 1.0 \
                 --top_k -1 \
                 --repetition_penalty 1.0 \
-                --batch_size 16 \
+                --batch_size 32 \
                 --seed 42
         fi
     done
